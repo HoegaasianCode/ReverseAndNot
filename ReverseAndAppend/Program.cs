24b101dfb15d3c43cb16cadebc8ad61a64c85694
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-21 19:42:47 +0100

    Initial commit

---
+++ ReverseAndAppend/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ReverseAndAppend
+{
+    class Program
+    {
+        // https://edabit.com/challenge/YGhgctqPsKQxQQCFS
+        // SOLVED
+
+        static void Main(string[] args)
+        {
+            Reverser reverse = new("123456789"); // -> "987654321123456789"
+            Console.WriteLine(reverse.ReverseString());
+        }
+    }
+}
---
+++ ReverseAndAppend/Reverser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseAndAppend
+{
+    public class Reverser
+    {
+        private readonly string _ints;
+
+        public Reverser(string ints)
+        {
+            _ints = ints;
+        }
+
+        public string ReverseString()
+        {
+            StringBuilder reverseInts = new();
+            for (int i = _ints.Length - 1; i >= 0; i--)
+            {
+                char c = _ints[i];
+                reverseInts.Append(c);
+            }
+            reverseInts.Append(_ints);
+            return reverseInts.ToString();
+        }
+    }
+}
